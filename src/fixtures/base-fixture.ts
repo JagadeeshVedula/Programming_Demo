9f80d99e30fdbbbf927e68dba2e5b06c9314aa30
Jagadeesh <vedulajagadeesh1994@gmail.com>
2026-07-15 12:57:48 +0530

    fixture updated

--- src/fixtures/base-fixture.ts
+++ src/fixtures/base-fixture.ts
@@ -1,14 +1,12 @@
 import {test as base} from '@playwright/test'
 import { BasePage } from '../pages/base-page'
 import { LoginPage } from '../pages/login-page'
-import { ContactUsPage } from '../pages/contactUs-page'
 import { APIContext } from '../pages/api-page'
 import { Add_Products } from '../pages/product-page'
 import { createCustomLogger } from '../utils/custom-logger'
 type PageObjects = {
     Base: BasePage;
     Login : LoginPage;
-    Contact : ContactUsPage;
     API : APIContext;
     Products: Add_Products
 }
@@ -21,10 +19,6 @@ export const test = base.extend<PageObjects>({
     Login: async({page},use)=>{
         const log = createCustomLogger(base.info().title)
         return use(new LoginPage(page,log))
-    },
-    Contact: async({page},use)=>{
-        const log = createCustomLogger(base.info().title)
-        return use(new ContactUsPage(page,log))
     },
     API: async({request},use)=>{
         const log = createCustomLogger(base.info().title)
